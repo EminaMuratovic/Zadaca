2b7f8e106b1497ce535fafb386697a4b4966e766
EminaMuratovic <emina_peace@hotmail.com>
2014-11-24 11:11:25 +0100

    ..

--- src/PresjekSkupa.java
+++ src/PresjekSkupa.java
@@ -1,32 +1,34 @@
 
-public class PresjekSkupa {
-	public static void main(String[] args) {
-		int a, b, c, d;
-		System.out.println("Unesite granice skupa S1!");
-		a = TextIO.getlnInt();
-		b = TextIO.getlnInt();
-		System.out.println("Unesite granice skupa S2!");
-		c = TextIO.getlnInt();
-		d = TextIO.getlnInt();
-		String pr = "";
-		if (a < c && b > d) {
-			System.out.println("Skup S2 je sadrzan u skupu S1.");
-		}
-		if (a > c && b < d) {
-			System.out.println("Skup S1 je sadrzan u skupu S2.");
-		}
-		if (a < c && b < d) {
-			for (int i = b; i <= d; i++) {
-				pr += i + " ";
-			}
-			System.out.println("Presjek skupova je: " + pr);
-		}
-		if (c < a && d < b) {
-			for (int i = a; i <= d; i++) {
-				pr += i + " ";
-			}
-			System.out.println("Presjek skupova je: " + pr);
-		}
-	}
 
+public class PresjekSkupa {
+
+	public static void main(String[] args) {
+	
+		
+		int a,b,c,d;
+		
+		System.out.println("Skup S1=[a,b].\nUnesi a: ");
+		a=TextIO.getInt();
+		System.out.println("Unesi b:");
+		b=TextIO.getInt();
+		
+		System.out.println("Skup S2=[c,d].\nUnesi c: ");
+		c=TextIO.getInt();
+		System.out.println("Unesi d:");
+		d=TextIO.getInt();
+		
+		if((a<c && b<d && a<c && b<c)||(a>c && b>d && a>c && b>c)){
+			System.out.println("Skupovi S1 i S2 se ne sijeku!");
+			return;
+		}
+		
+		if(a<c&&b>d) System.out.println("Skup S2 je sadržan u skupu S1!");
+		
+		if(c<a&&d>b) System.out.println("Skup S1 je sadržan u skupu S2!");
+		
+		if(a>c&&d<b) System.out.printf("Presjek skupova S1 i S2 je skup: [%d,%d]",a,d);
+		if(c>a&&b<d) System.out.printf("Presjek skupova S1 i S2 je skup: [%d,%d]",c,b);
+		
+	}
 }
+
